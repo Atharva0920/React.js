e64a6b8935f781d6720a200d9070cd4990e2a61e
Ganmote <aganmote@ptc.com>
2024-01-05 23:37:00 +0530

    React-2

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # React.js
-Learn and practice react.js 
+- Learn and practice react.js 🚀
+
+
+# Parcel Provides ->
+- Zero Config
+- Dev Build
+- Local Server
+- Caching in .parcel-cache
+- HMR [Hot Modeule Replacement ]
+- File Watching Algorithm - written in c++
+- Image Optimization
+- Minification
+- Bundling 
+- Compressing using .Gzip
+- Consistent Hashing
+- Code Splitting
+- Differential Budling  - Support older Browser
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking Algorithm - [ Remove unused code for us ] if there are 10 functions and only 6 of them are being used this algo will remove unused algos.
+- different build for dev and prod 
--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello React!");
-console.log(heading); // Object
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const heading = React.createElement("h1",{id:"heading", class :"ad"},"Hello Devs👨‍💻!");
+const subHeading = React.createElement("h1",{id:"sub-heading",class:"ad"},"Learning React?🚀");
+const newHeading = React.createElement("h1",{id:"new-heading", class:"ad"},"me too😎")
+const parent = React.createElement("div",{id:"parent"},[heading, subHeading, newHeading]); // Object
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
